9c59582800f246c3d5bcf18e5038d78e33400d29
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-12-15 10:40:45 -0500

    Add simple python call from cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Python.h"
+
+int main()
+{
+    Py_Initialize();
+    PyRun_SimpleString("from time import time,ctime\n"
+                       "print('Today is', ctime(time()))\n");
+    Py_FinalizeEx();
+    
+    return 0;
+}
