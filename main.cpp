d06a3eb02ab0d1c769a7f243df15582f02f8b547
Gabriel Motta <gabrielbenmotta@gmail.com>
2022-12-19 11:24:49 -0500

    Accessing python from cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include "Python.h"
+#include <iostream>
+
+#include <python3.10/Python.h>
+#include <vector>
 
 int main()
 {
+    std::cout << "Hello from C++!\n";
+
     Py_Initialize();
-    PyRun_SimpleString("from time import time,ctime\n"
-                       "print('Today is', ctime(time()))\n");
-    Py_FinalizeEx();
-    
+
+    PyRun_SimpleString("print('Hello from Python!')");
+    PyRun_SimpleString("i = 10");
+
+    PyObject *mainModule = PyImport_AddModule("__main__");
+    PyObject *var_i = PyObject_GetAttrString(mainModule, "i");
+
+    int i = PyLong_AS_LONG(var_i);
+
+    std::cout <<"var i is " << i << "\n";
+
+    Py_Finalize();
+
     return 0;
 }
