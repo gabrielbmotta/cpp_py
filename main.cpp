55d960f1fc328c93e3e2425f7896fc4821a3e586
gabrielbmotta <gabrielbenmotta@gmail.com>
2023-02-10 11:51:26 -0500

    add module and function from cpp and call it

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <Python.h>
 #include <iostream>
 
+PyObject *add_func(PyObject *self, PyObject *args) {
+  int a, b;
+  if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
+    return NULL;
+  }
+  return PyLong_FromLong(a + b);
+}
+
+static PyMethodDef my_module_methods[] = {
+    {"add", add_func, METH_VARARGS, "Add two integers."},
+    {NULL, NULL, 0, NULL}};
+
+static PyModuleDef my_module_def = {PyModuleDef_HEAD_INIT,
+                                    "my_module",
+                                    "An empty Python module",
+                                    -1,
+                                    my_module_methods,
+                                    NULL,
+                                    NULL,
+                                    NULL,
+                                    NULL};
+
+PyMODINIT_FUNC PyInit_my_module(void) {
+  return PyModule_Create(&my_module_def);
+}
+
 int main() {
+  PyImport_AppendInittab("my_module", &PyInit_my_module);
+
   Py_Initialize();
-  PyRun_SimpleString("def add(a, b):\n\treturn a + b");
-  PyObject *pModule = PyImport_AddModule("__main__");
+
+  PyObject *pModule = PyImport_ImportModule("my_module");
+
   PyObject *pFunc = PyObject_GetAttrString(pModule, "add");
-
-  PyObject *pArgs = PyTuple_New(2);
-  PyTuple_SetItem(pArgs, 0, PyLong_FromLong(1));
-  PyTuple_SetItem(pArgs, 1, PyLong_FromLong(2));
-
+  PyObject *pArgs = PyTuple_Pack(2, PyLong_FromLong(1), PyLong_FromLong(2));
   PyObject *pValue = PyObject_CallObject(pFunc, pArgs);
+
   long result = PyLong_AsLong(pValue);
 
   std::cout << "The result is: " << result << "\n";
